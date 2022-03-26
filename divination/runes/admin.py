75b0d81c99c132f7691dec1c833dc85d39a15224
Yuriy Peskov <yuriy.peskov@gmail.com>
2022-03-26 09:17:48 +0200

    added admin views for Divination model

--- divination/runes/admin.py
+++ divination/runes/admin.py
@@ -3,5 +3,8 @@ from django.contrib import admin
 from runes.models import Divination
 
 
-class AuthorAdmin(admin.ModelAdmin):
-    admin.site.register(Divination)
+# admin.site.register(Author, AuthorAdmin)
+@admin.register(Divination)
+class DivinationAdmin(admin.ModelAdmin):
+    list_display = ('order', 'title', 'description')
+    ordering = ('order',)
--- divination/runes/models.py
+++ divination/runes/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class Divination(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True)
+    order = models.PositiveIntegerField(unique=True, null=True)
+
+    def __str__(self) -> str:
+        return f'{self.title}'
