88c3e30c025b593b5cf6ffee2fd265de90032724
Yuriy Peskov <yuriy.peskov@gmail.com>
2022-03-31 11:28:54 +0300

    started i18n and l10n

--- divination/divination/settings.py
+++ divination/divination/settings.py
@@ -112,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ua'
 
 TIME_ZONE = 'UTC'
 
@@ -120,6 +120,10 @@ USE_I18N = True
 
 USE_TZ = True
 
+LOCALE_PATHS = [
+    BASE_DIR / 'locale',
+    # BASE_DIR / 'pages' / 'locale'
+]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
